0ce671c5f0829bc22664b14a1b2d672be8ae7794
Jessica <jkelly@bluespiral.io>
2025-05-14 13:20:11 -0400

    create hero styles and hero-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgFor
   ],
   providers: [
     provideClientHydration(withEventReplay())
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
-import { Hero } from '../hero';
+import { NgFor, NgIf } from '@angular/common';
 
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { FormsModule } from '@angular/forms';
+  
 @Component({
   selector: 'app-heroes',
   standalone: false,
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  //hero: Hero = {
+  //  id: 1,
+  //  name: 'Windstorm'
+  //}
 
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  };
 }
