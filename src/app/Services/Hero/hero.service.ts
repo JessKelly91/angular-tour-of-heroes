abf4cadf696c533ab9f4e8c8cbc24581f0024afb
Jessica <jkelly@bluespiral.io>
2025-05-16 13:13:24 -0400

    update Http error handling in HeroService

--- src/app/Services/Hero/hero.service.ts
+++ src/app/Services/Hero/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from '../../Models/hero';
 import { HEROES } from '../../Models/MockModels/mock-heroes';
@@ -23,8 +24,25 @@ export class HeroService {
     this.messageService.add(`HeroService: ${message}`);
   }
 
+  handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  };
+
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   };
 
   getHero(id: number): Observable<Hero> {
